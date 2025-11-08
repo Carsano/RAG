fd8f474ba7c88f28a5dfa23fd1018376b239291c
Carsano <damien.fabiano@protonmail.com>
2025-11-08 09:24:05 +0100

    Add vectore_store port

--- interface/ports/vector_store.py
+++ interface/ports/vector_store.py
@@ -2,3 +2,11 @@
 Abstract vector store interface.
 Contains methods for adding, searching, and managing vectors.
 """
+from typing import List, Tuple, Protocol
+
+
+class VectorStore(Protocol):
+    def search(self, query_embedding: list[float], k: int = 10
+               ) -> Tuple[list[int], list[float]]: ...
+
+    def get_chunks(self, ids: List[int]) -> List[str]: ...
