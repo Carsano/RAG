7655870cb5edc3a5f9be3655067e89219f9270f9
Carsano <damien.fabiano@protonmail.com>
2025-11-11 12:26:04 +0100

    Create util files for get project root

---
+++ src/rag/utils/utils.py
@@ -0,0 +1,17 @@
+"""
+Utility functions for the RAG project.
+"""
+from __future__ import annotations
+import pathlib
+
+
+def get_project_root() -> pathlib.Path:
+    """Get the root directory of the project.
+
+    Returns:
+        pathlib.Path: Path to the project root.
+    """
+    root = pathlib.Path(__file__).resolve()
+    while root.name != "src":
+        root = root.parent
+    return root.parent
