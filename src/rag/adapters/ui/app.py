5794a5d53904c43b5128c78eb0312a37929c60e8
Carsano <damien.fabiano@protonmail.com>
2025-11-27 20:48:41 +0100

    Add app.py structure

--- src/rag/adapters/ui/app.py
+++ src/rag/adapters/ui/app.py
@@ -1,3 +1,20 @@
 """
 Entry point Streamlit
 """
+import streamlit as st
+from pages import chat
+
+PAGES = {
+    "Chat": chat
+}
+
+
+def main():
+    with st.sidebar:
+        choice = st.radio("Navigation", list(PAGES.keys()))
+
+    PAGES[choice].render()
+
+
+if __name__ == "__main__":
+    main()
